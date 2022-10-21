ebd60cd1ebe1cf82bd83b5699599ebf615895031
kstajbnz <k.stajkowski@o2.pl>
2022-10-21 15:10:14 +0200

    zadeklarownie i poustawienie

--- app/src/main/java/com/taboret/mobilek/MainActivity.kt
+++ app/src/main/java/com/taboret/mobilek/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.taboret.mobilek
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val dopisz = findViewById<Button>(R.id.dopisz)
+        val przywroc = findViewById<Button>(R.id.przywroc)
+        val teskt = findViewById<TextView>(R.id.text)
+        val wpisz = findViewById<EditText>(R.id.wpisz)
     }
 }
